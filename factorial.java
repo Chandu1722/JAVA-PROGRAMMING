a82337ac9bd3e32074e5ddca1dae5987d4b29cc2
Chandu1722 <chendichandu.com@gmail.com>
2025-03-05 20:29:39 +0530

    2025-03-05 factorial.java
    Factorial using methods

---
+++ factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Factorial{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter a number:");
+    int num=sc.nextInt();
+    sc.close();
+    Factorial f=new Factorial();
+    System.out.print("The factorial of given number is "+f.fact(num));
+  }
+  int fact(int x){
+    if(x==0){
+      return 1;
+    }
+    return x*fact(x-1);
+  }
