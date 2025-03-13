53a4306cc44fdd5584c8db8327aa09c6b4f936bc
Chandu1722 <chendichandu.com@gmail.com>
2025-03-13 21:48:43 +0530

    2025-03-13 Hierarchical_Inheritance.java

---
+++ Hierarchical_Inheritance.java
@@ -0,0 +1,26 @@
+class parent{
+  void print(){
+    System.out.println("This is parent class");
+  }
+}
+class child1 extends parent{
+  void printChild1(){
+    System.out.println("This is child1 class");
+  }
+}
+class child2 extends parent{
+  void printChild2(){
+    System.out.println("This is child2 class");
+  }
+}
+class Hierarchical_Inheritance{
+  public static void main(String[] args){
+    child1 c1=new child1();
+    child2 c2=new child2();
+    c1.printChild1();
+    c1.print();
+    c2.printChild2();
+    c2.print();
+  }
+}
+
