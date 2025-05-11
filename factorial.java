1fabfcff8d2dfbe4256bd9a98026a06fadea6ba7
Chandu1722 <chendichandu.com@gmail.com>
2025-05-11 21:06:50 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -4,7 +4,7 @@ class Factorial{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     System.out.print("Enter a number:");
-    int num=sc.nextInt();
+    int num=sc.nextInt(); 
     sc.close();
     Factorial f=new Factorial();
     System.out.print("The factorial of given number is "+f.fact(num));
